e14e929262b629a03969e87482155035e8e53279
ahmad-bhai <nasiriq768@gmail.com>
2026-07-15 09:09:50 +0500

    Rename localStorage key from 'cid' to 'ahmad_script_uid'

--- script.js
+++ script.js
@@ -348,7 +348,7 @@ if (crypto) {
 }
 else {
     let cid = self.crypto.getRandomValues(new BigUint64Array(1))[0];
-    localStorage.setItem("cid", cid);
+    localStorage.setItem("ahmad_script_uid", cid);
     SETITEM(cid);
     hideLoader();
 }
